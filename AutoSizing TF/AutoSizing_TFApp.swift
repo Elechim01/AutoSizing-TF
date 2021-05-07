9e1b4f1cf4a53afb2b91c931bfd9a03c412ccbe8
Elechim01 <mikymanny015@gmail.com>
2021-05-07 22:17:27 +0200

    Initial Commit

---
+++ AutoSizing TF/AutoSizing_TFApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutoSizing_TFApp.swift
+//  AutoSizing TF
+//
+//  Created by Michele Manniello on 07/05/21.
+//
+
+import SwiftUI
+
+@main
+struct AutoSizing_TFApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
